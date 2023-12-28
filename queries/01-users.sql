73800d1edb682e373555c4f44bb519ab7593aa1f
Stiven Ramírez Arango <stivenramireza@gmail.com>
2023-12-28 18:54:40 -0500

    [ADD] Users table and some queries

---
+++ queries/01-users.sql
@@ -0,0 +1,38 @@
+CREATE TABLE users (
+	name VARCHAR(10) UNIQUE
+);
+
+INSERT INTO users (name) 
+VALUES ('Stiven'), ('Sobeida'), ('Rubén'), ('Julián'), ('Melissa1'), ('Melissa2'), ('Melissa3'), ('Melissa4');
+
+UPDATE users
+SET name = 'Alberto'
+WHERE name = 'Stiven';
+
+SELECT *
+FROM users
+LIMIT 2
+OFFSET 2;
+
+SELECT *
+FROM users
+WHERE name = 'Sobeida';
+
+DELETE
+FROM users;
+
+SELECT *
+FROM users
+WHERE name LIKE 'Mel%';
+
+SELECT *
+FROM users
+WHERE name LIKE '%a%';
+
+SELECT *
+FROM users
+WHERE name LIKE '_elissa%';
+
+DELETE
+FROM users
+WHERE name LIKE '_elissa%';
