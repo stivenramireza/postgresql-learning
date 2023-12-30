634a16081595c83a747fb9b2a15110405f658c11
Stiven Ramírez Arango <stivenramireza@gmail.com>
2023-12-29 21:44:40 -0500

    [ADD] First task

---
+++ tasks/01-task.sql
@@ -0,0 +1,41 @@
+-- Tasks
+
+-- 1. Ver todos los registros
+SELECT *
+FROM users;
+
+-- 2. Ver el registro cuyo id sea igual a 10
+SELECT *
+FROM users
+WHERE id = 10;
+
+-- 3. Quiero todos los registros que cuyo primer nombre sea Jim (engañosa)
+SELECT *
+FROM users
+WHERE SPLIT_PART(name, ' ', 1) = 'Jim';
+
+SELECT *
+FROM users
+WHERE name LIKE 'Jim %';
+
+-- 4. Todos los registros cuyo segundo nombre es Alexander
+SELECT *
+FROM users
+WHERE SPLIT_PART(name, ' ', 2) = 'Alexander';
+
+SELECT *
+FROM users
+WHERE name LIKE '% Alexander';
+
+-- 5. Cambiar el nombre del registro con id = 1, por tu nombre Ej:'Fernando Herrera'
+UPDATE users
+SET name = 'Stiven Ramírez'
+WHERE id = 1;
+
+-- 6. Borrar el último registro de la tabla
+DELETE
+FROM users
+WHERE id = (
+    SELECT MAX(id)
+	FROM users
+);
