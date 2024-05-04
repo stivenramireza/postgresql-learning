4d8cfce0cc890fa79f404535121dc24171981094
Stiven Ramírez Arango <stivenramireza@gmail.com>
2024-05-04 18:11:31 -0500

    [ADD] Materialized views

---
+++ classes/28-materialized_views.sql
@@ -0,0 +1,23 @@
+CREATE MATERIALIZED VIEW comments_per_week_mat AS (
+	SELECT 
+		DATE_TRUNC('week', p.created_at) AS weeks, 
+		COUNT(DISTINCT(p.post_id)) AS number_of_posts,
+		SUM(c.counter) AS total_claps,
+		COUNT(*) AS number_of_claps
+	FROM posts AS p
+	INNER JOIN claps AS c ON c.post_id = p.post_id
+	GROUP BY weeks
+	ORDER BY weeks DESC
+);
+
+SELECT *
+FROM comments_per_week;
+
+SELECT *
+FROM comments_per_week_mat;
+
+REFRESH MATERIALIZED VIEW comments_per_week_mat;
+
+SELECT *
+FROM posts
+WHERE post_id = 1;
